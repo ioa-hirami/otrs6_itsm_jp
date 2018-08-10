d127c423dd3e8fdeec2af3765c76d5128abbf4b1
hirami <hirami@io-architect.com>
2018-08-10 12:00:52 +0900

    6.0.10

--- README.txt
+++ README.txt
@@ -4,23 +4,24 @@ OTRS6 Helpdeskに追加モジュール及びITSM関連モジュールを追加�
 
 各モジュールのバージョンは以下の通りです。
 
-OTRS 6.0.8
+OTRS 6.0.10
 
 - OTRS Addons -
-FAQ-6.0.7.opm
-Survey-6.0.4.opm
-SystemMonitoring-6.0.2.opm
-TimeAccounting-6.0.4.opm
-OTRSMasterSlave-6.0.6.opm
+FAQ-6.0.9.opm
+Survey-6.0.5.opm
+SystemMonitoring-6.0.3.opm
+TimeAccounting-6.0.5.opm
+OTRSMasterSlave-6.0.8.opm
+
 
 - ITSM -
-GeneralCatalog-6.0.8.opm
-ImportExport-6.0.8.opm
-ITSMCore-6.0.8.opm
-ITSMChangeManagement-6.0.8.opm
-ITSMConfigurationManagement-6.0.8.opm
-ITSMIncidentProblemManagement-6.0.8.opm
-ITSMServiceLevelManagement-6.0.8.opm
+GeneralCatalog-6.0.10.opm
+ImportExport-6.0.10.opm
+ITSMCore-6.0.10.opm
+ITSMChangeManagement-6.0.10.opm
+ITSMConfigurationManagement-6.0.10.opm
+ITSMIncidentProblemManagement-6.0.10.opm
+ITSMServiceLevelManagement-6.0.10.opm
 
 実行は以下のようにしてください。
 docker run -d -p <ポート>:80 docker.io/ioarchitectjp/otrs5_itsm_jp
--- instpkg.sh
+++ instpkg.sh
@@ -2,16 +2,16 @@
 
 CMD=/opt/otrs/bin/otrs.Console.pl
 
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:FAQ-6.0.7.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:Survey-6.0.4.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:SystemMonitoring-6.0.2.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:TimeAccounting-6.0.4.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:OTRSMasterSlave-6.0.6.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:FAQ-6.0.9.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:Survey-6.0.5.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:SystemMonitoring-6.0.3.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:TimeAccounting-6.0.5.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:OTRSMasterSlave-6.0.8.opm
 
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:GeneralCatalog-6.0.8.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:ImportExport-6.0.8.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:ITSMCore-6.0.8.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:ITSMChangeManagement-6.0.8.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:ITSMConfigurationManagement-6.0.8.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:ITSMIncidentProblemManagement-6.0.8.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:ITSMServiceLevelManagement-6.0.8.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:GeneralCatalog-6.0.10.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:ImportExport-6.0.10.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:ITSMCore-6.0.10.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:ITSMChangeManagement-6.0.10.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:ITSMConfigurationManagement-6.0.10.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:ITSMIncidentProblemManagement-6.0.10.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages6/:ITSMServiceLevelManagement-6.0.10.opm
